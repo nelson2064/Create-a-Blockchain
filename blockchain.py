4d6988d87e4c7fee697f576d55189571e8c8f044
Nelson Parajuli <121667396+nelson2064@users.noreply.github.com>
2023-03-26 18:45:46 +0545

    Checking if the Blockchain is valid
    Checking if the Blockchain is valid

--- blockchain.py
+++ blockchain.py
@@ -102,3 +102,12 @@
                             response = {'chain': blockchain.chain,
                                         'length': len(blockchain.chain)}
                             return jsonify(response), 200
+ # Checking if the Blockchain is valid
+                        @app.route('/is_valid', methods = ['GET'])
+                        def is_valid():
+                            is_valid = blockchain.is_chain_valid(blockchain.chain)
+                            if is_valid:
+                                response = {'message': 'All good. The Blockchain is valid.'}
+                            else:
+                                response = {'message': 'Houston, we have a problem. The Blockchain is not valid.'}
+                            return jsonify(response), 200
